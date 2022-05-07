b7e7ce559fd72b4ed1527ef3641bdc1e67dfdf9a
AmoreiraT <thiago.m.hq@gmail.com>
2022-05-07 12:00:05 -0300

    up

--- src/core/firebase/firebase.js
+++ src/core/firebase/firebase.js
@@ -1,21 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBzmqqgP7T484p6at2tmq1XQTBsaWjo7Qg",
-  authDomain: "thmoreiralab-ec20b.firebaseapp.com",
-  projectId: "thmoreiralab-ec20b",
-  storageBucket: "thmoreiralab-ec20b.appspot.com",
-  messagingSenderId: "690088339458",
-  appId: "1:690088339458:web:c5bb9aea7c552fe8e79f1b",
-  measurementId: "G-P9B41YSVWT",
+  apiKey: process.env.REACT_APP_FB_API_KEY,
+  authDomain: process.env.REACT_APP_FB_AUTHD,
+  projectId: process.env.REACT_APP_FB_PID,
+  storageBucket: process.env.REACT_APP_FB_SBKT,
+  messagingSenderId: process.env.REACT_APP_FB_MSID,
+  appId: process.env.REACT_APP_FB_APPID,
+  measurementId: process.env.REACT_APP_FB_FB_MID,
 };
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+console.log(analytics);
